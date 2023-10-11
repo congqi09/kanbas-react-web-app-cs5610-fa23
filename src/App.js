457106beb2255a77e5d37ea2ac7a8d8b127a1d95
Cong Qi <qi.co@northeastern.edu>
2023-10-10 21:39:22 -0700

    2.1.7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Labs from './Labs';
 import HelloWorld from './Labs/a3/HellloWorld'
+import Kanbas from './Kanbas'
 
 function App() {
   return (
     <div>
       <HelloWorld />
       <Labs />
+      <Kanbas />
     </div>
   );
 }
