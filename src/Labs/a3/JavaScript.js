de309583dd3bc89f753f0153b3a857d62c5fd44d
Cong Qi <qi.co@northeastern.edu>
2023-10-13 22:52:14 -0700

    2.2.fin

--- src/Labs/a3/JavaScript.js
+++ src/Labs/a3/JavaScript.js
@@ -7,8 +7,10 @@ import WorkingWithFunctions from "./WorkingWithFunctions";
 import WorkingWithArrays from "./WorkingWithArrays";
 import JsonStringify from "./JsonStringify";
 import TemplateLiterals from "./TemplateLiterals";
+import Destructing from "./Destructing";
 import House from "./House";
 import Spread from "./Spread";
+import FunctionDestructing from "./FunctionDestructing";
 function JavaScript() {
     console.log('Hello World!');
     return(
@@ -25,6 +27,8 @@ function JavaScript() {
             <House />
             <TemplateLiterals />
             <Spread />
+            <Destructing />
+            <FunctionDestructing />
         </div>
     );
 }
