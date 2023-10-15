5e3c8ff22e969c93cdd9135127d6e5519488c3ef
Cong Qi <qi.co@northeastern.edu>
2023-10-14 23:51:24 -0700

    2.4.fin

---
+++ src/Labs/a3/Classes/index.js
@@ -0,0 +1,22 @@
+import './index.css'    
+function Classes() {    
+    return ( 
+        <div class="wd-padding-10px   
+            wd-bg-green">
+            <h2>Classes</h2>    
+            <div className="wd-fg-black
+             wd-bg-red
+             wd-padding-10px
+            ">  
+                Red background
+            </div> 
+            <div className="wd-bg-yellow wd-fg-black wd-padding-10px">         
+                Yellow background 
+            </div> 
+            <div className="wd-bg-blue wd-fg-black wd-padding-10px">         
+                Blue background   
+            </div> 
+    </div>
+    )
+}
+export default Classes
--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,13 +1,14 @@
-import JavaScript from "./JavaScript";
-import WorkingWithArrays from "./WorkingWithArrays";
 import PathParameters from "./PathParameters";
+import JavaScript from "./JavaScript"  
+import Classes from "./Classes"
 function Assignment3() {
     return (
         <div>
            <h1>Assignment 3</h1>
+           <Classes />     
            <JavaScript />
            <PathParameters />
         </div>
     );
 }
-export default Assignment3
+export default Assignment3;
