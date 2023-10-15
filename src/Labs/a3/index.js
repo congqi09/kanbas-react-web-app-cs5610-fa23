e561ecc5ab8a3355129010f55b6f88e4346a6363
Cong Qi <qi.co@northeastern.edu>
2023-10-15 12:53:00 -0700

    202310151252PT prepare jumpping throughe buhole

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -2,10 +2,12 @@ import PathParameters from "./PathParameters";
 import JavaScript from "./JavaScript"  
 import Classes from "./Classes"
 import Styles from "./Styles"
+import ConditionalOutput from "./ConditionalOutput";
 function Assignment3() {
     return (
         <div>
            <h1>Assignment 3</h1>
+           <ConditionalOutput />
            <Styles />
            <Classes />     
            <JavaScript />
