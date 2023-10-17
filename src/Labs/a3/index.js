b9fb2e4a7a98357c91ce84f498fb069d245a8a51
Cong Qi <qi.co@northeastern.edu>
2023-10-16 21:13:14 -0700

    2.fin

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,15 +1,17 @@
-import PathParameters from "./PathParameters";
+import PathParameters from "./PathParameters" 
 import JavaScript from "./JavaScript"  
-import Classes from "./Classes"
-import Styles from "./Styles"
+import Classes from "./Classes" 
+import Styles from "./Styles"  
+import TodoList from "./todo/TodoList" 
 function Assignment3() {
     return (
         <div>
            <h1>Assignment 3</h1>
            <Styles />
            <Classes />     
-           <JavaScript />
-           <PathParameters />
+           <TodoList />
+           {/* <JavaScript /> */}
+           {/* <PathParameters /> */} 
         </div>
     );
 }
