5b2ae24fdd2e9c0696cfed26968b0690189aeeb1
Cong Qi <qi.co@northeastern.edu>
2023-10-20 14:03:05 -0700

    3.2

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,5 +1,7 @@
 import Nav from "../Nav"
 import KanbasNavigation from "./KanbasNavigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 function Kanbas() {
     return(
         <div className="container">
@@ -7,9 +9,15 @@ function Kanbas() {
             <div className="d-flex">
                 <KanbasNavigation/>
                 <div>
-                    <h1>Account</h1>
+                    {/* <h1>Account</h1>
                     <h1>Dashboard</h1>
-                    <h1>Courses</h1>
+                    <h1>Courses</h1> */}
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Dashboard" />} />
+                        <Route path="Account" element={<h1>Account</h1>} />
+                        <Route path="Dashboard" element={<Dashboard />} />
+                        <Route path="Courses/*" element={<h1>Courses</h1>} />
+                    </Routes>
                 </div>
             </div>
         </div>
