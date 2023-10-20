d1b1f2e767fad10f69f532637ed22f49c39738f9
Cong Qi <qi.co@northeastern.edu>
2023-10-20 14:31:37 -0700

    3.4

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,11 +1,34 @@
 import db from "../../Kanbas/Database";
-import { useParams } from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import CourseNavigation from "./CourseNavigation";
 function Courses() {
     const { courseId } = useParams();
     const course = db.courses.find((course) => course._id === courseId);
     return (
         <div>
-        <h1>Course {course.name}</h1>
+            <h1>Course {course.name}</h1>
+            <CourseNavigation />
+            <div>
+                <div
+                    className="overflow-y-scroll position-fixed bottom-0 end-0"
+                    style={{
+                        left: "430px",
+                        top: "100px",
+                    }}
+                >
+                <Routes>
+                    <Route path="/" element={<Navigate to="Home" />} />
+                    <Route path="Home" element={<h1>Home</h1>} />
+                    <Route path="Modules" element={<h1>Modules</h1>} />
+                    <Route path="Assignments" element={<h1>Assignments</h1>} />
+                    <Route
+                        path="Assignments/:assignmentId"
+                        element={<h1>Assignment Editor</h1>}
+                    />
+                    <Route path="Grades" element={<h1>Grades</h1>} />
+                </Routes>
+                </div>
+            </div>
         </div>
     );
 }
