d21b6a0cfeaeeb3ce3a9c3663774b13556ebb808
Cong Qi <qi.co@northeastern.edu>
2023-10-21 15:18:10 -0700

    3.6

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -2,6 +2,7 @@ import db from "../../Kanbas/Database";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
+import Home from "./Home";
 function Courses() {
     const { courseId } = useParams();
     const course = db.courses.find((course) => course._id === courseId);
@@ -19,7 +20,7 @@ function Courses() {
                 >
                 <Routes>
                     <Route path="/" element={<Navigate to="Home" />} />
-                    <Route path="Home" element={<h1>Home</h1>} />
+                    <Route path="Home" element={<Home />} />
                     <Route path="Modules" element={<Modules/>} />
                     <Route path="Assignments" element={<h1>Assignments</h1>} />
                     <Route
