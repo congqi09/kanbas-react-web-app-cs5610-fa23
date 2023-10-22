1e3d7073b0a73e95d616caffb3a737fbe64755f3
Cong Qi <qi.co@northeastern.edu>
2023-10-21 17:28:42 -0700

    2.2.1 - A4

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React from "react"
+import Add from "./Add"
 function Assignment4() {
     return (
         <div>
            <h1>Assignment 4</h1>
+           <Add a={1} b={2} />
            {/* <Styles />
            <Classes />     
            <TodoList />
