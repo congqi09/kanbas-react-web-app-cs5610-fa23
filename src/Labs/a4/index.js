2d4493ab40fe48bdd5682326fe290f0ab5ce085b
Cong Qi <qi.co@northeastern.edu>
2023-10-22 11:57:50 -0700

    2.2.5

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -3,15 +3,17 @@ import React from "react"
 // import ClickEvent from "./ClickEvent"
 // import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 function Assignment4() {
     const sayHello = () => alert("Hello")
     return (
         <div>
-           <h1>Assignment 4</h1>
-           <PassingFunctions theFunction={sayHello} />
-           {/* <Add a={1} b={2} />
-           <ClickEvent/>
-           <PassingDataOnEvent /> */}
+            <h1>Assignment 4</h1>
+            <PassingFunctions theFunction={sayHello} />
+            {/* <Add a={1} b={2} />
+            <ClickEvent/>
+            <PassingDataOnEvent /> */}
+            <EventObject />
         </div>
     );
 }
