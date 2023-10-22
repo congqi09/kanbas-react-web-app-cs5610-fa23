79d301984c44780d1d32e857bbfc8a416fe56d3f
Cong Qi <qi.co@northeastern.edu>
2023-10-21 17:25:26 -0700

    A4 - 2.1

---
+++ src/Labs/a4/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Assignment4() {
+    return (
+        <div>
+           <h1>Assignment 4</h1>
+           {/* <Styles />
+           <Classes />     
+           <TodoList />
+           <JavaScript />
+           {/* <PathParameters /> */}
+        </div>
+    );
+}
+export default Assignment4
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,11 +1,18 @@
 import Assignment3 from "./a3";
-import Nav from "../Nav";
+import Nav from "../Nav"
+import Assignment4 from "./a4"
+import {Routes, Route, Navigate} from "react-router";
 function Labs() {
     return(
         <div className="container">
             <Nav/>
-            <Assignment3/>
+            <Routes>
+                <Route path="/" element={<Navigate to="a3"/>}/>
+                <Route path="a3" element={<Assignment3/>} />
+                <Route path="a4" element={<Assignment4/>} />
+            </Routes>
+            {/* <Assignment3/> */}
         </div>
-    );
-}
+    )
+};
 export default Labs;
