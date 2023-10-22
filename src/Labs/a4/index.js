9b7040657c0a5086626c3d29da938f4463a2bd6f
Cong Qi <qi.co@northeastern.edu>
2023-10-21 17:33:24 -0700

    A4 - 2.2.2

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,15 +1,12 @@
 import React from "react"
-import Add from "./Add"
+import Add from "./Add";
+import ClickEvent from "./ClickEvent"
 function Assignment4() {
     return (
         <div>
            <h1>Assignment 4</h1>
            <Add a={1} b={2} />
-           {/* <Styles />
-           <Classes />     
-           <TodoList />
-           <JavaScript />
-           {/* <PathParameters /> */}
+           <ClickEvent/>
         </div>
     );
 }
