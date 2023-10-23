53724b71606ff8920cf4e991c09f99433a2cbfbb
Cong Qi <qi.co@northeastern.edu>
2023-10-22 21:34:35 -0700

    2.4.1 add redux

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,32 +1,39 @@
 import React from "react"
+import ReduxExamples from "./ReduxExamples"
 // import Add from "./Add";
 // import ClickEvent from "./ClickEvent"
 // import PassingDataOnEvent from "./PassingDataOnEvent";
-import PassingFunctions from "./PassingFunctions";
-import EventObject from "./EventObject";
-import Counter from "./Counter";
-import BooleanStateVariables from "./BooleanStateVariables";
-import StringStateVariables from "./StringStateVariables";
-import DateStateVariable from "./DateStateVariable";
-import ObjectStateVariable from "./ObjectStateVariable";
-import ArrayStateVariable from "./ArrayStateVariable";
-import ParentStateComponent from "./ParentStateComponent";
+// import PassingFunctions from "./PassingFunctions";
+// import EventObject from "./EventObject";
+// import Counter from "./Counter";
+// import BooleanStateVariables from "./BooleanStateVariables";
+// import StringStateVariables from "./StringStateVariables";
+// import DateStateVariable from "./DateStateVariable";
+// import ObjectStateVariable from "./ObjectStateVariable";
+// import ArrayStateVariable from "./ArrayStateVariable";
+// import ParentStateComponent from "./ParentStateComponent";
 function Assignment4() {
-    const sayHello = () => alert("Hello")
+    // const sayHello = () => alert("Hello")
+    // return (
+    //     <div>
+    //         <h1>Assignment 4</h1>
+    //         <PassingFunctions theFunction={sayHello} />
+    //         {/* <Add a={1} b={2} /><ClickEvent/><PassingDataOnEvent /> */}
+    //         <EventObject />
+    //         <Counter />
+    //         <BooleanStateVariables />
+    //         <StringStateVariables />
+    //         <DateStateVariable />
+    //         <ObjectStateVariable />
+    //         <ArrayStateVariable />
+    //         <ParentStateComponent />
+    //     </div>
+    // )
     return (
-        <div>
-            <h1>Assignment 4</h1>
-            <PassingFunctions theFunction={sayHello} />
-            {/* <Add a={1} b={2} /><ClickEvent/><PassingDataOnEvent /> */}
-            <EventObject />
-            <Counter />
-            <BooleanStateVariables />
-            <StringStateVariables />
-            <DateStateVariable />
-            <ObjectStateVariable />
-            <ArrayStateVariable />
-            <ParentStateComponent />
-        </div>
+        <>
+            <h1>Assignment4</h1>
+            <ReduxExamples />
+        </>
     )
 }
 export default Assignment4
