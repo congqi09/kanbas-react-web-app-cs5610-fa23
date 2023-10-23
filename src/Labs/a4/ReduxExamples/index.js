2b7986f5be0c2dfda50b438927c89e388a2a01f6
Cong Qi <qi.co@northeastern.edu>
2023-10-23 13:48:40 -0700

    2.4.3

--- src/Labs/a4/ReduxExamples/index.js
+++ src/Labs/a4/ReduxExamples/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
 const ReduxExamples = () => {
     return(
         <div>
             <h2>Redux Examples</h2>
             <HelloRedux />
+            <CounterRedux />
         </div>
     );
 };
