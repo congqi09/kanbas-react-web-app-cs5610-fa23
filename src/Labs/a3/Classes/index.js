de8f32a738b5c390fb491b53d76ba26d777ee2fa
Cong Qi <qi.co@northeastern.edu>
2023-10-24 19:41:49 -0700

    fix another a3 bug

--- src/Labs/a3/Classes/index.js
+++ src/Labs/a3/Classes/index.js
@@ -1,7 +1,7 @@
 import './index.css'    
 function Classes() {    
     return ( 
-        <div class="wd-padding-10px   
+        <div className="wd-padding-10px   
             wd-bg-green">
             <h2>Classes</h2>    
             <div className="wd-fg-black
