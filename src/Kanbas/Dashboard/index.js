607de1e301b96901e0cd5a57d8a1f3dece02537f
Cong Qi <qi.co@northeastern.edu>
2023-10-31 13:07:55 -0700

    3.1.3

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -10,9 +10,19 @@ function Dashboard() {
     const addNewCourse = () => {
         setCourses([...courses, {...course, _id: new Date().getTime() }]);
     };
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <h5>Course</h5>
+            <input value={course.name} className="form-control"
+                   onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+            <input value={course.number} className="form-control"
+                   onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+            <input value={course.startDate} className="form-control" type="date"
+                   onChange={(e) => setCourse({ ...course, startDate: e.target.value }) } />
+            <input value={course.endDate} className="form-control" type="date"
+                   onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
             <button onClick = {addNewCourse} > Add </button>
             {/* todo: add some styles? */}
             <div className="list-group">
