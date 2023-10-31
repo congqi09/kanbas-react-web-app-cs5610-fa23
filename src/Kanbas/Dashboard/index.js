c32287de4799bdc13cb26ed2ad3a076259741044
Cong Qi <qi.co@northeastern.edu>
2023-10-31 13:01:05 -0700

    3.1.3

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import db from "../Database";
 function Dashboard() {
+    const [courses, setCourses] = useState(db.courses);
+    const [course, setCourse] = useState({
+        name: "New Course", number: "04356",
+        startDate: "2023-11-01", endDate: "2023-10-31",
+    });
+    const addNewCourse = () => {
+        setCourses([...courses, {...course, _id: new Date().getTime() }]);
+    };
     return (
         <div>
             <h1>Dashboard</h1>
+            <button onClick = {addNewCourse} > Add </button>
             {/* todo: add some styles? */}
             <div className="list-group">
-                {db.courses.map((course) => (
+                {courses.map((course) => (
                     <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
                         {course.name}
                     </Link>
