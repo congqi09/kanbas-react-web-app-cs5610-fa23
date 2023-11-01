35488cfc778ae42c80e48e6f7d59e603c7001611
Cong Qi <qi.co@northeastern.edu>
2023-10-31 21:12:09 -0700

    3.3.1

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 function ModuleList() {
     const { courseId } = useParams();
-    const modules = db.modules.filter(
-        (module) => module.course === courseId);
+    const [modules, setModules] = useState(db.modules);
+    const [module, setModule] = useState({
+        name: "New Module",
+        description: "New Description",
+        course: courseId,
+    });
+    const addModule = (module) => {
+        setModules([
+            { ...module, _id: new Date().getTime().toString() },
+            ...modules,
+        ])
+    }
     return (
         <ul className="list-group">
-            {
-                modules.map((module, index) => (
+            <li className="list-group-item">
+                <button onClick={() => { addModule(module) }}> Add </button>
+                <input value={module.name}
+                    onChange={(e) => setModule({
+                    ...module, name: e.target.value })}
+                />
+                <textarea value={module.description}
+                    onChange={(e) => setModule({
+                    ...module, description: e.target.value })}
+                />
+            </li>
+            {modules
+                .filter(m => m.course === courseId)
+                .map((module, index) => (
                     <li key={index} className="list-group-item">
                         <h3>{module.name}</h3>
                         <p>{module.description}</p>
+                        <p>{module._id}</p>
                     </li>
                 ))
             }
