e9599aeb6703e48f47f7acbd1bf04554aee1a238
Cong Qi <qi.co@northeastern.edu>
2023-11-07 17:08:48 -0800

    A5 - 3.0

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,8 +1,9 @@
-import Assignment3 from "./a3";
-import Nav from "../Nav"
-import Assignment4 from "./a4"
 import {Routes, Route, Navigate} from "react-router";
+import Nav from "../Nav"
+import Assignment3 from "./a3";
 import store from "./store";
+import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import { Provider } from "react-redux";
 function Labs() {
     return(
@@ -10,11 +11,11 @@ function Labs() {
             <div className="container">
                 <Nav/>
                 <Routes>
-                    <Route path="/" element={<Navigate to="a3"/>}/>
+                    <Route path="/" element={<Navigate to="a4"/>}/>
                     <Route path="a3" element={<Assignment3/>} />
                     <Route path="a4" element={<Assignment4/>} />
+                    <Route path="a5" element={<Assignment5/>} />
                 </Routes>
-                {/* <Assignment3/> */}
             </div>
         </Provider>
     );
